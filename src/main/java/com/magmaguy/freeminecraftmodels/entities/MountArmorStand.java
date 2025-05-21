43f4c95d39a9cfb822ef8a9979543dc6dce44994
Endcrypt <pternej32@gmail.com>
2025-05-22 07:39:45 +0800

    update: used nms to support server-side teleporting while a passenger is mounted

--- src/main/java/com/magmaguy/freeminecraftmodels/entities/MountArmorStand.java
+++ src/main/java/com/magmaguy/freeminecraftmodels/entities/MountArmorStand.java
@@ -1,15 +1,11 @@
 package com.magmaguy.freeminecraftmodels.entities;
 
-import com.github.retrooper.packetevents.PacketEvents;
-import com.github.retrooper.packetevents.protocol.player.User;
-import com.github.retrooper.packetevents.wrapper.play.server.WrapperPlayServerEntityTeleport;
 import com.magmaguy.freeminecraftmodels.customentity.core.Skeleton;
 import de.tr7zw.changeme.nbtapi.NBT;
-import io.github.retrooper.packetevents.util.SpigotConversionUtil;
 import lombok.Getter;
 import lombok.Setter;
-import org.bukkit.Bukkit;
 import org.bukkit.Location;
+import org.bukkit.craftbukkit.v1_21_R1.entity.CraftEntity;
 import org.bukkit.entity.ArmorStand;
 
 @Setter
@@ -45,21 +41,13 @@ public class MountArmorStand {
     }
 
     public void move(Location newLocation) {
-        int entityId = armorStand.getEntityId();
-
         // Update the stored location
         this.location = newLocation;
 
-        WrapperPlayServerEntityTeleport teleportPacket = new WrapperPlayServerEntityTeleport(
-                entityId,
-                SpigotConversionUtil.fromBukkitLocation(newLocation),
-                false
-        );
 
-        Bukkit.getOnlinePlayers().forEach(player -> {
-           User user = PacketEvents.getAPI().getPlayerManager().getUser(player);
-           user.sendPacket(teleportPacket);
-        });
+        CraftEntity craftEntity = (CraftEntity) armorStand;
+        craftEntity.getHandle().a(newLocation.getX(), newLocation.getY(), newLocation.getZ(), newLocation.getYaw(), newLocation.getPitch());
+
     }
 
     public void destroy() {
